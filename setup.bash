aa747d3b5c7203881172221599c5c0c7ec4a0dd0
Gabriel <gabrielblomberg0@gmail.com>
2020-08-08 12:31:48 +1000

    Added a bash script to install GLEW and GLFW locally automatically

---
+++ setup.bash
@@ -0,0 +1,10 @@
+echo "This script installs flew and glfw locally in this folder."
+
+mkdir lib
+cd lib
+
+echo "Installing GLEW under lib/glew/}"
+git clone https://github.com/nigels-com/glew.git
+
+echo "Installing GLFW under lib/glfw/"
+git clone https://github.com/glfw/glfw.git
